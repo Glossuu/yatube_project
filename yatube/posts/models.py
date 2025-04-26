c027c19d8c4060622c0cd91e9de3a4fe07173bb7
Алексей <lebedew.alexej2017@yandex.ru>
2025-04-26 12:59:12 +0300

    Финальный коммит

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    group = models.ForeignKey(
+        'Group', on_delete=models.CASCADE, blank=True, null=True
+    )
+
+
+class Group(models.Model):
+    title = models.TextField()
+    slug = models.SlugField(default='Пусто')
+    description = models.TextField(default='Пусто')
+
+    def str(self):
+        return self.title
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,15 +1,43 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+# from django.http import HttpResponse
+from .models import Post, Group
 
 # Главная страница
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    title = 'Последние обновления на сайте'
+    # Одна строка вместо тысячи слов на SQL:
+    # в переменную posts будет сохранена выборка из 10 объектов модели Post,
+    # отсортированных по полю pub_date по убыванию (от больших значений к меньшим)
+    posts = Post.objects.order_by('-pub_date')[:10]
+    # В словаре context отправляем информацию в шаблон
+    context = {
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, template, context)
+
+# Страница со списком всевозможных групп
+
+
+def group_list(request, slug):
+    title = 'Записи сообщества'
+    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'title': title,
+        'posts': posts,
+        'group': group,
+    }
+    return render(request, template, context)
 
 
 # Страница с информацией о сообществе;
 # view-функция принимает параметр name из path()
 
-def group_posts(request, name):
-    return HttpResponse(f'Наименование сообщества {name}')
+
+# def group_posts(request, name):
+    # return HttpResponse(f'Наименование сообщества {name}')
